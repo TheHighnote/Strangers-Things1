b9a8b9933e3f2c7b18008cd83748b46ecce2a2a0
TheHighnote <ryanhighnote7@gmail.com>
2023-02-20 12:28:22 -0600

    added to main.jsx, can now retrive posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Navbar } from "./";
+import getPosts from "../API-Adapt";
 
 const Main = () => {
+    const [posts, setPosts] = useState([])
+
+    async function retrievePosts () {
+        const myPosts = await getPosts()
+        setPosts (myPosts.data.posts)
+        console.log(myPosts)
+
+    }
+    useEffect(() => {
+        retrievePosts()
+    }, [])
+
+
     return(
         <div id="main">
             <Navbar />
