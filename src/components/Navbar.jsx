c1cb7e9e4639492a833dec36ce99aa2f5a2d19dc
TheHighnote <ryanhighnote7@gmail.com>
2023-02-23 12:00:19 -0600

    Added new post button to Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,9 @@ const Navbar = () => {
   return (
     <div id="navbar">
       <Link to={"/"}><h2> Stranger's Things</h2></Link>
-      <Link to={"/Login"}><button>Login</button></Link>
-      <Link to={"/Register"}><button>Register</button></Link>
+      <Link to={"/Login"}><button>Log in</button></Link>
+      <Link to={"/Register"}><button>Sign up</button></Link>
+      <Link to={"/CreatePost"}><button>New Post</button></Link>
     </div>
   );
 };
