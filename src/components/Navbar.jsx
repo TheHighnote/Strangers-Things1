2493df426b53560318d7130688555ca24e805eab
TheHighnote <ryanhighnote7@gmail.com>
2023-02-23 14:14:32 -0600

    more css added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   return (
     <div id="navbar">
       <Link to={"/"} id="title"><h2> Stranger's Things</h2></Link>
+      <div id="nav-buttons">
       <Link to={"/Login"}><button>Log in</button></Link>
       <Link to={"/Register"}><button>Sign up</button></Link>
       <Link to={"/CreatePost"}><button>New Post</button></Link>
+      </div>
     </div>
   );
 };
